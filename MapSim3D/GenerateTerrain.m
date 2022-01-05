19c8b485e00dbba843bad5d9d0ce4a416ea2ca11
coreymarcus <corey.marcus@gmail.com>
2022-01-05 13:45:06 -0800

    completed approximation of ALHAT data

--- MapSim3D/GenerateTerrain.m
+++ MapSim3D/GenerateTerrain.m
@@ -4,11 +4,15 @@ clc
 
 % Setup terrain parameters
 rng(3)
-width = 200;
-height = 200;
+width = 80;
+height = 80;
+xmin = -40;
+ymin = -40;
+xmax = xmin + width;
+ymax = ymin + height;
 gsd = .1;
-xsample = 0:gsd:width;
-ysample = 0:gsd:height;
+xsample = xmin:gsd:xmax;
+ysample = ymin:gsd:ymax;
 Nx = length(xsample);
 Ny = length(ysample);
 groundA = 1E-4;
@@ -22,20 +26,56 @@ craterRadiusMean = 10;
 craterRadiusStdDev = 0.5;
 craterAbundance = 0.08;
 
-% Find the number of rocks and craters
-terrainArea = width*height;
-numRocks = round(rockAbundance*terrainArea/(pi*rockRadiusMean^2));
-numCraters = round(craterAbundance*terrainArea/(pi*craterRadiusMean^2));
+% Use ALHAT approximate data?
+useALHAT = true;
 
-% Sample rock and crater paremeters
-rockX = width*rand([numRocks,1]);
-rockY = height*rand([numRocks,1]);
-rockRadius = abs(mvnrnd(rockRadiusMean,rockRadiusStdDev^2,numRocks));
-
-craterX = width*rand([numCraters,1]);
-craterY = height*rand([numCraters,1]);
-craterRadius = abs(mvnrnd(craterRadiusMean,craterRadiusStdDev^2,numCraters));
-craterDepth = abs(mvnrnd(craterDepthToWidthMean,craterDepthToWidthStdDev^2,numCraters).*2.0.*craterRadius);
+if useALHAT
+    % Read rocks
+    rocks = xlsread('ApproxALHATRocks.xlsx');
+    
+    % Extract data
+    numRocks = size(rocks,1);
+    rockX = rocks(:,1);
+    rockY = rocks(:,2);
+    rockRadius = rocks(:,3);
+    
+    % read craters
+    craters = xlsread('ApproxALHATCraters.xlsx');
+    
+    % Extract data
+    numCraters = size(craters,1);
+    craterX = craters(:,1);
+    craterY = craters(:,2);
+    craterRadius = craters(:,3);
+    craterDepth = craters(:,4);
+    
+    % Calculate surface normal parameters
+    rise = 1.0;
+    run = 60;
+    nhat = [-rise 0 run]';
+    nhat = nhat/norm(nhat);
+    offset = 0;
+    
+else
+    % Find the number of rocks and craters
+    terrainArea = width*height;
+    numRocks = round(rockAbundance*terrainArea/(pi*rockRadiusMean^2));
+    numCraters = round(craterAbundance*terrainArea/(pi*craterRadiusMean^2));
+    
+    % Sample rock and crater paremeters
+    rockX = width*rand([numRocks,1]);
+    rockY = height*rand([numRocks,1]);
+    rockRadius = abs(mvnrnd(rockRadiusMean,rockRadiusStdDev^2,numRocks));
+    
+    craterX = width*rand([numCraters,1]);
+    craterY = height*rand([numCraters,1]);
+    craterRadius = abs(mvnrnd(craterRadiusMean,craterRadiusStdDev^2,numCraters));
+    craterDepth = abs(mvnrnd(craterDepthToWidthMean,craterDepthToWidthStdDev^2,numCraters).*2.0.*craterRadius);
+    
+    % Use a flat ground plane
+    nhat = [0 0 1]';
+    offset = 0;
+end
 
 % Generate rock plots
 rockDEM = zeros(Ny,Nx);
@@ -47,7 +87,7 @@ for ii = 1:numRocks
     rockSample = gsd*(-cellRad:cellRad);
     Nrock = length(rockSample);
     rockDEMiter = zeros(Nrock);
-
+    
     % Create rock height
     for jj = 1:Nrock
         for kk = 1:Nrock
@@ -58,17 +98,17 @@ for ii = 1:numRocks
             rockDEMiter(jj,kk) = sqrt(rad^2 - rockSample(jj)^2 - rockSample(kk)^2);
         end
     end
-
+    
     % Add rock to large DEM
-    rockSampleX = round((rockSample + x)/gsd);
-    rockSampleY = round((rockSample + y)/gsd);
+    rockSampleX = round((rockSample + x - xmin)/gsd);
+    rockSampleY = round((rockSample + y - ymin)/gsd);
     targX = rockSampleX >= 0 & rockSampleX < Nx;
     targY = rockSampleY >= 0 & rockSampleY < Ny;
     rockDEMiter = rockDEMiter(targY, targX);
     rockDEM(rockSampleY(targY) + 1,rockSampleX(targX) + 1) = ...
         max(rockDEM(rockSampleY(targY) + 1,rockSampleX(targX) + 1), ...
         rockDEMiter);
-
+    
 end
 
 % Generate crater plots
@@ -82,11 +122,11 @@ for ii = 1:numCraters
     craterSample = gsd*(-cellRad:cellRad);
     Ncrater = length(craterSample);
     craterDEMiter = zeros(Ncrater);
-
+    
     % Find z location of crater sphere center
     R = (depth^2 + 0.25*(2*rad)^2)/(2*depth);
     z = R - depth;
-
+    
     % Create crater height
     for jj = 1:Ncrater
         for kk = 1:Ncrater
@@ -97,21 +137,29 @@ for ii = 1:numCraters
             craterDEMiter(jj,kk) = z - sqrt(R^2 - craterSample(jj)^2 - craterSample(kk)^2);
         end
     end
-
+    
     % Add crater to large DEM
-    craterSampleX = round((craterSample + x)/gsd);
-    craterSampleY = round((craterSample + y)/gsd);
+    craterSampleX = round((craterSample + x - xmin)/gsd);
+    craterSampleY = round((craterSample + y - ymin)/gsd);
     targX = craterSampleX >= 0 & craterSampleX < Nx;
     targY = craterSampleY >= 0 & craterSampleY < Ny;
     craterDEMiter = craterDEMiter(targY, targX);
     craterDEM(craterSampleY(targY) + 1,craterSampleX(targX) + 1) = ...
         min(craterDEM(craterSampleY(targY) + 1,craterSampleX(targX) + 1), ...
         craterDEMiter);
+    
+end
 
+% Generate ground plane DEM
+groundDEM = zeros(Ny,Nx);
+for ii = 1:Nx
+    for jj = 1:Ny
+        groundDEM(jj,ii) = (offset - ii*nhat(1)*gsd - jj*nhat(2)*gsd)/nhat(3);
+    end
 end
 
 % Create final DEM by adding rocks and craters together
-rockAndCraterDEM = craterDEM + rockDEM;
+rockAndCraterDEM = craterDEM + rockDEM + groundDEM;
 
 % Save DEM to disk
 save('RockAndCraterDEM','rockAndCraterDEM',"gsd");
@@ -132,7 +180,8 @@ legend('Rocks','Craters')
 % mesh(craterDEM)
 % title('Crater DEM')
 
-% figure
-% mesh(xsample,ysample,rockAndCraterDEM)
-% title('Combined DEM')
-% view(2)
+figure
+mesh(xsample,ysample,rockAndCraterDEM)
+title('Combined DEM')
+colorbar
+view(2)
