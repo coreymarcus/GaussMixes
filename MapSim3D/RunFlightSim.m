2d04a8524030e605a24854fb714b1b80ba72321a
coreymarcus <corey.marcus@gmail.com>
2022-01-06 12:43:01 -0800

    config changes

--- MapSim3D/RunFlightSim.m
+++ MapSim3D/RunFlightSim.m
@@ -43,7 +43,7 @@ Rmeas = (0.08^2)*eye(3);
 %other settings
 truthgsd = .5;
 somemeasonly = false; % run entire trajectory or just first measurment
-somemeastarg = 10;
+somemeastarg = 15;
 
 % tree settings
 switch terrain
